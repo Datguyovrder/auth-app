cb7e7c2924785947dd199748dd6da54a8761b154
Richard Hui <rhui@hawk.iit.edu>
2018-09-15 11:42:30 -0500

    part 2 completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
-    post "/users" => "users#create"
+    post "/sessions" => 'sessions#create'
+
+    post "/users" => 'users#create'
   end
 end
